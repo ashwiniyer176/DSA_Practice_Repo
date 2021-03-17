8d847395734f663acc6d26308ad27b44f1e92af5
Ashwin U Iyer <ashwiniyer1706@gmail.com>
2021-03-17 21:14:05 +0530

    strings commit

--- sets.py
+++
@@ -1,9 +0,0 @@
-l1 = set()
-l1.add(11)
-l1.add(5)
-l1.add(7)
-print(l1, len(l1))
-for i in l1:
-    print(i)
-x = l1.pop()
-print(x, l1)
--- tuples.py
+++
@@ -1,9 +0,0 @@
-l = [("a", 11), ("b", 2), ("c", 3)]
-print(l)
-small = l[0]
-for node in l:
-    if(node[-1] < small[-1]):
-        small = node
-l.remove(small)
-
-print(l)
