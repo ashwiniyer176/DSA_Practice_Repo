4756af21c78d578ab113c2537e35453aa628f746
Ashwin U Iyer <ashwiniyer1706@gmail.com>
2022-08-31 00:04:04 +0530

    Knapsack problem DP2

--- Dynamic Proramming/DP_2_Knapsack.py
+++ Dynamic Proramming/DP_2_Knapsack.py
@@ -8,11 +8,13 @@ mat = [[0 for i in range(knapsack_wt+1)] for j in range(num_of_items+1)]
 
 for i in range(1, num_of_items+1):
     for j in range(1, knapsack_wt+1):
-        maxValueWithoutCurrent = mat[i-1][j]
-        maxValueWithCurrent = 0
-        if weights[i-1] <= j:
+        maxValueWithoutCurrent = mat[i-1][j]  # This value will always exist
+        maxValueWithCurrent = 0  # The value we actually need to calculate
+        if weights[i-1] <= j:  # If the current item's weight is less than knapsack's capacity
             maxValueWithCurrent = values[i-1]
             remainingCapacity = j-weights[i-1]
+            # Fill the remaining space with the max possible value
             maxValueWithCurrent += mat[i-1][remainingCapacity]
+        # Select the better option
         mat[i][j] = max(maxValueWithCurrent, maxValueWithoutCurrent)
 print(f"Max possible values are:{mat[num_of_items][knapsack_wt]}")
